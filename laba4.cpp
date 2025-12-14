3ef113df5b131619d7c7ea7107db2865c99b9fe1
MAX <tagirov_maksimka_91@mail.ru>
2025-12-14 23:09:05 +0500

    add gitignore and sleket

--- laba4.cpp
+++ laba4.cpp
@@ -1,10 +1,30 @@
-﻿// laba4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
+#include <string>
 
-#include <iostream>
+// Функция 1: Чтение строк из файла в вектор
+std::vector<std::string> readFromFile(const std::string& filename) {
+    std::vector<std::string> lines;
+    // TODO: Реализовать чтение из файла
+    std::cout << "[DEBUG] readFromFile called. Returning empty vector.\n";
+    return lines;
+}
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// Функция 2: Вывод вектора строк на экран
+void printToScreen(const std::vector<std::string>& lines) {
+    // TODO: Реализовать вывод на экран
+    std::cout << "[DEBUG] printToScreen called. Nothing printed.\n";
+}
+
+// Функция 3: Запись вектора строк в файл
+void writeToFile(const std::vector<std::string>& lines, const std::string& filename) {
+    // TODO: Реализовать запись в файл
+    std::cout << "[DEBUG] writeToFile called. Nothing written.\n";
+}
+
+int main() {
+    std::vector<std::string> data = readFromFile("input.txt");
+    printToScreen(data);
+    writeToFile(data, "output.txt");
     return 0;
 }
